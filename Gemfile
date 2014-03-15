ce7db2709736ff463936fec0f534747794d9c6eb
Peter Compernolle <me@petercompernolle.com>
2014-03-15 11:44:21 -0500

    adds sinatra assetpack and app directory structure

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@
 source :rubygems
 
 gem 'sinatra'
+gem 'thin'
+gem 'therubyracer'
+gem 'sinatra-assetpack', :require => 'sinatra/assetpack'
--- app.rb
+++ app.rb
@@ -2,12 +2,38 @@
 # encoding: UTF-8
 # run with the command 'rackup'
 require 'sinatra/base'
+require 'sinatra/assetpack'
 
 class OutOfSomething < Sinatra::Base
   set :root, File.dirname(__FILE__) # You must set app root
   configure do
     set :site_name, "Out of Something, LLC"
   end
+
+  register Sinatra::AssetPack
+
+  assets {
+    serve '/js',     from: 'app/js'        # Default
+    serve '/css',    from: 'app/css'       # Default
+    serve '/fonts',  from: 'app/fonts'     # Default
+    serve '/images', from: 'app/images'    # Default
+
+    # The second parameter defines where the compressed version will be served.
+    # (Note: that parameter is optional, AssetPack will figure it out.)
+    # The final parameter is an array of glob patterns defining the contents
+    # of the package (as matched on the public URIs, not the filesystem)
+    js :app, '/js/app.js', [
+      '/js/vendor/**/*.js',
+      '/js/lib/**/*.js'
+    ]
+
+    css :application, '/css/application.css', [
+      '/css/*.css'
+    ]
+
+    js_compression  :jsmin
+    css_compression :simple
+  }
 
   get '/' do
     erb :index, :locals => {}
